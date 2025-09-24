f27f6d48a429811250010ec6ab4e8336d825d90c
JeanBonatto <jb.bonatto@outlook.com>
2025-09-24 19:28:14 -0300

    :sparkles: melhorado o validador, incluido mais validações com as configs

--- src/processor/validator.py
+++ src/processor/validator.py
@@ -1,21 +1,38 @@
 from fastapi import HTTPException, status
+class ValidatorConfig:
+    def __init__(self):
+        self.fields_identificador = ["id", "name"]
+        self.fields_opcional = ["weight", "height"]
+
 
 class Validator:
-    pass
+    def __init__(self, config: ValidatorConfig = None):
+        self.config = config or ValidatorConfig()
 
-    def is_validate_identifier_field(self,identifier: str | int, field: str = None) -> bool:
-        #Verifica se o identinficador é um FLoat e retorna falso
-        if isinstance(identifier, float):
-            return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
-                                 detail="Identificador inválido, o mesmo não pode ser um número decimal.")
+    def is_validate_identifier_field(self,identifier: str | int, field: str = None) -> str:
+        '''
+        Método para validar o identificador e o campo field
+        Args:
+            identifier (str | int): Identificador do Pokemon (ID ou nome)
+            field (str, optional): Campo de busca ["id", "name", "weight", "height"]. Defaults to None.
+        '''
         
-        # Verifica se o identificador é um número em formato string e converte para int
-        if isinstance(identifier, str) and identifier.isdigit():
-            identifier = int(identifier)
-        #Verifica o campo field, se possui valor valido
-        if field:
-            '''
-            Necessário verificar quais as opções validas desse campos
-            '''
-            pass
-        return True
+        try:
+            #Verifica se o identinficador é um FLoat e retorna falso
+            if isinstance(identifier, float):
+                return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                                    detail="Identificador inválido, o mesmo não pode ser um número decimal.")
+            
+            # Verifica se o identificador é um número em formato string e converte para int
+            if isinstance(identifier, str) and identifier.isdigit():
+                identifier = int(identifier)
+            #Verifica o campo field, se possui valor valido
+            if field:
+                field = field.lower().strip()
+                if field not in self.config.fields and field not in self.config.fields_opcional:
+                    return HTTPException(status_code=status.HTTP_400_BAD_REQUEST, 
+                                        detail=f"Campo inválido, os campos válidos são: {', '.join(self.config.fields)}")
+            return identifier, field
+        except Exception as e:
+            return HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+                                 detail=str(e))
