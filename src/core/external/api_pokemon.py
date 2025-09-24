6bb1578367bd709c865f20fcd0dbc024dbab1c1f
JeanBonatto <jb.bonatto@outlook.com>
2025-09-23 21:22:44 -0300

    :sparkles: feat: Adicionado Classe para para request da api pokemon base

---
+++ src/core/external/api_pokemon.py
@@ -0,0 +1,31 @@
+from requests import request
+class PokemonConfig:
+    def __init__(self):
+        self.url = 'https://pokeapi.co/api/v2/pokemon/'
+
+class PokemonAPI:
+    def __init__(self, config: PokemonConfig):
+        self.config = config
+    
+    def __call__(self, *args, **kwds):
+        return self.get_id_name(*args, **kwds)
+    
+    def get_id_name(self, name: str = None) -> dict:
+        '''
+        Esse método faz a requisição para a API do Pokemon e retorna o JSON com as informações do Pokemon.
+        Segue o link da API: https://pokeapi.co/docs/v2#pokemon
+
+        Args:
+            name (str, optional): Nome do Pokemon. Defaults to None.
+        Returns:
+            dict: Retorna um dicionário com as informações do Pokemon.
+        '''
+        try:
+            if not name:
+                return None
+            req = request('GET', self.config.url + name)
+            return req.json()
+        
+        except Exception as e:
+            print(f'Error in request API Pokemon:{self.config.url + name}. Erro:{e}')
+            return None
